e25e3f0978cf99db631c0e6ef6e3d28c5418b578
Nisa Najafli <ncfliniss@gmail.com>
2025-11-22 00:28:55 +0400

    update

--- assests/main.js
+++ assests/main.js
@@ -13,3 +13,20 @@ var swiper = new Swiper(".reviewMainSwiper", {
 
     speed: 700,
 });
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  if (window.scrollY > 150) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+// window.addEventListener("scroll", () => {
+//   const header = document.getElementById("site-header");
+//   if (window.scrollY > 250) {        
+//     header.classList.add("scrolled");
+//   } else {
+//     header.classList.remove("scrolled");
+//   }
+// });
+document.getElementById("year").textContent = new Date().getFullYear()
